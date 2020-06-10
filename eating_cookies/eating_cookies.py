7bcfd968b699793edad2beb4dc5120fbd279a0b3
MicahJank <MicahJank@users.noreply.github.com>
2020-06-10 00:13:28 -0500

    working on cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,6 +1,14 @@
 '''
 Input: an integer
 Returns: an integer
+
+Cookie Monster can eat either 1, 2, or 3 cookies at a time. If he were given a jar of cookies with n cookies inside of it, 
+how many ways could he eat all n cookies in the cookie jar? Implement a function eating_cookies that counts the number of 
+possible ways Cookie Monster can eat all of the cookies in the jar.
+
+UNDERSTAND
+- n is a number that represents how many cookies are in the jar
+- 
 '''
 def eating_cookies(n):
     # Your code here
--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,3 +1,4 @@
+import math
 '''
 Input: a List of integers
 Returns: a List of integers
