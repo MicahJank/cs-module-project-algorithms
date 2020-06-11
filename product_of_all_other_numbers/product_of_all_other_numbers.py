4ccf630c37e49cd98aaf03c637fc890478ac272e
Micah <micahjank@gmail.com>
2020-06-11 14:22:34 -0500

    product of all others

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -22,21 +22,22 @@ UPDATE
 - then you loop through the new array and divide each value in the array by its corresponding value in the first array
 - assign the new value to the position at that index
 
-TODO - cant figure this out will need to come back to it
+UPDATE UPDATE
+- can use math.prod to multiply all the items in the list together
+after that i can loop over the array and divide the values by their counterpart
 '''
 def product_of_all_other_numbers(arr):
-    new_arr = arr.copy()
-    for i in range(len(arr)):
-        for j in range(len(new_arr)):
-            if i != j:
-                new_arr[i] *= arr[j]
+    no_zeroes = [num for num in arr if num != 0]
+    no_zero_num = math.prod(no_zeroes)
+    num = math.prod(arr)
+    prod_arr = [num] * len(arr)
 
-    print(new_arr)
     for i in range(len(arr)):
-        if new_arr[i] == 0:
-            arr[i] = 0
+        
+        if arr[i] == 0:
+            arr[i] = no_zero_num
         else:
-            arr[i] = new_arr[i] // arr[i]
+            arr[i] = prod_arr[i] // arr[i]
 
     return arr
                 
@@ -44,7 +45,15 @@ def product_of_all_other_numbers(arr):
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [0, 2, 3, 0, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+
+
+'''
+REFLECT
+- this implementation does not work if there is more than 1 zero in the array
+- i was able to get the time complexity to O(n) but space complexity is also O(n)
+- could try to find a way to get the time complexity to O(1)
+'''
